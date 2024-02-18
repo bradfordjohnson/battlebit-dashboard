fa1d9c4246414155339077f9ac630c7741405f39
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2024-02-18 16:50:55 -0500

    fixed formatting

--- scripts/logData.js
+++ scripts/logData.js
@@ -20,26 +20,26 @@ const data = {
             this.observations.push(newObservation);
         }
     },
-    readJsonFile: function (filename) {
+    readJsonFile: function (fileName) {
         try {
-            const data = fs.readFileSync(filename, 'utf8');
+            const data = fs.readFileSync(fileName, 'utf8');
             const jsonData = JSON.parse(data);
             this.observations = jsonData.observations || [];
         } catch (err) {
             console.error(err);
         }
     },
-    saveJsonFile: function (filename) {
+    saveJsonFile: function (fileName) {
         try {
             const data = JSON.stringify({ observations: this.observations }, null, 2);
-            fs.writeFileSync(filename, data);
-            console.log('Data has been saved to', filename);
+            fs.writeFileSync(fileName, data);
+            console.log('Data has been saved to', fileName);
         } catch (err) {
             console.error(err);
         }
     },
-    fetchDataAndUpdate: async function (filename, url) {
-        this.readJsonFile(filename);
+    fetchDataAndUpdate: async function (fileName, url) {
+        this.readJsonFile(fileName);
         try {
             const newData = await json(url);
             newData.forEach(item => { this.add(item) });
@@ -49,4 +49,4 @@ const data = {
     }
 };
 
-data.fetchDataAndUpdate('data.json', 'https://publicapi.battlebit.cloud/Servers/GetServerList');
+data.fetchDataAndUpdate('historicServerData.json', 'https://publicapi.battlebit.cloud/Servers/GetServerList');
