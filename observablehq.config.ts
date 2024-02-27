a3dd857c7a2010f6b7d7436e329bd98942000dce
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2024-02-26 19:27:11 -0500

    update header

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -3,27 +3,15 @@ export default {
   // The project’s title; used in the sidebar and webpage titles.
   title: "Battlebit Dashboard",
   pager: false,
-  toc: false
-
-  // The pages and sections in the sidebar. If you don’t specify this option,
-  // all pages will be listed in alphabetical order. Listing pages explicitly
-  // lets you organize them into sections and have unlisted pages.
-  // pages: [
-  //   {
-  //     name: "Examples",
-  //     pages: [
-  //       {name: "Dashboard", path: "/example-dashboard"},
-  //       {name: "Report", path: "/example-report"}
-  //     ]
-  //   }
-  // ],
-
-  // Some additional configuration options and their defaults:
-  // theme: "default", // try "light", "dark", "slate", etc.
-  // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
-  // toc: true, // whether to show the table of contents
-  // pager: true, // whether to show previous & next links in the footer
-  // root: "docs", // path to the source root for preview
-  // output: "dist", // path to the output root for build
+  toc: false,
+  header: `<div style="display: flex; align-items: center; gap: 0.5rem; height: 2.2rem; margin: -1.5rem -2rem 2rem -2rem; padding: 0.5rem 2rem; border-bottom: solid 1px var(--theme-foreground-faintest); font: 500 16px var(--sans-serif);">
+  <div style="display: flex; flex-grow: 1; justify-content: space-between; align-items: baseline;">
+    <a href="https://www.ford-johnson.com/projects/">
+    Back to portfolio
+    </a>
+    <span style="display: flex; align-items: baseline; gap: 0.5rem; font-size: 14px;">
+      <a target="_blank" href="https://github.com/bradfordjohnson/portfolio"><span>GitHub</span></a>
+    </span>
+  </div>
+</div>`
 };
